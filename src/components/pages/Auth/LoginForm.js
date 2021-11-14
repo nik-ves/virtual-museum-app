0379376885726cbc3d76f664722f044e3bdee8e0
nik-ves <nik-ves@hotmail.com>
2021-11-14 18:01:41 +0100

    Login form

--- src/components/pages/Auth/LoginForm.js
+++ src/components/pages/Auth/LoginForm.js
@@ -1,5 +1,44 @@
+import classes from "./LoginForm.module.css";
+
+import useInput from "../../../hooks/use-input";
+
 const LoginForm = () => {
-  return <h1>Login form</h1>;
+  const { value: email, valueChangeHandler: emailChangeHandler } = useInput();
+  const { value: password, valueChangeHandler: passwordChangeHandler } =
+    useInput();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className={classes["form-content"]}>
+      <form onSubmit={submitHandler} className={classes.form}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={emailChangeHandler}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={passwordChangeHandler}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
