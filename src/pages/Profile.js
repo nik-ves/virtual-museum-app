b80631ffa5b5b0c852d4754b28fee065086bfc77
nik-ves <nik-ves@hotmail.com>
2021-12-06 00:05:44 +0100

    Fixed a route

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,18 @@
 import { AuthContext } from "../context/auth-context";
 import { useContext } from "react";
 
+import Container from "../components/UI/Container";
+
 const Profile = () => {
   const authCtx = useContext(AuthContext);
 
   return (
-    <div>
+    <Container>
       <h1>{authCtx.currentUser?.firstName}</h1>
       <h1>{authCtx.currentUser?.lastName}</h1>
       <h1>{authCtx.currentUser?.email}</h1>
       <h1>{authCtx.currentUser?.password}</h1>
-    </div>
+    </Container>
   );
 };
 
